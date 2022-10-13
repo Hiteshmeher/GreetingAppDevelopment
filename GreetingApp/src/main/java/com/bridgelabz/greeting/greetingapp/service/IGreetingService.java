e20634d32113ad5f3d218a0896d7a67b65e503c7
Hiteshmeher <meher.hitesh4@gmail.com>
2022-10-13 12:41:47 +0530

    [Hitesh]: Ability for Greeting app to list all Greeting Messag in the Repository

--- GreetingApp/src/main/java/com/bridgelabz/greeting/greetingapp/service/IGreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greeting/greetingapp/service/IGreetingService.java
@@ -3,9 +3,12 @@ package com.bridgelabz.greeting.greetingapp.service;
 import com.bridgelabz.greeting.greetingapp.entity.Greeting;
 import com.bridgelabz.greeting.greetingapp.entity.User;
 
+import java.util.List;
+
 public interface IGreetingService {
     String getGreetingMessage();
     String getGreetingMessage(User user);
     Greeting addGreetingMessage(User user);
     Greeting getGreetingMsgById(long id);
+    List<Greeting> getAllGreetingMsg();
 }
